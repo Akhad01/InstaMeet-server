9f0e1784ecd2ff96abdf1b301e620a92f44e031b
Akhad <abdurazakovahad@gmail.com>
2023-08-05 14:53:49 +0300

    I wrote the logic for registration

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,49 @@
 const express = require('express')
-
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const { generateUserData } = require('../utils/helpers')
+const tokenService = require('../services/token.service')
 const router = express.Router({ mergeParams: true })
 
-router.post('/signUp', async (req, res) => {})
+// /api/auth/signUp
+// 1) get data from req (email, password ...)
+// 2) check if user already exists
+// 3) hash password
+// 4) create user
+// 5) generate tokens
+
+router.post('/signUp', async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    const existingUser = await User.findOne({ email })
+
+    if (existingUser) {
+      return res.status(400).json({
+        error: {
+          message: 'EMAIL_EXISTS',
+          code: 400,
+        },
+      })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12)
+
+    const newUser = await User.create({
+      ...generateUserData(),
+      ...req.body,
+      password: hashedPassword,
+    })
+
+    const tokens = tokenService.generate({ _id: newUser._id })
+
+    res.status(201).send({ ...tokens, userId: newUser._id })
+  } catch (error) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже',
+    })
+  }
+})
 router.post('/signInWithPassword', () => {})
 router.post('/token', () => {})
 
